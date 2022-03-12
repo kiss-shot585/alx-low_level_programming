d1f42ec632c0e1aa57a00dd2ea8bd45cfe189183
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-03-12 01:28:17 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <time.h>
 /**
  * main - Entry point
@@ -8,6 +8,36 @@
  */
 int main(void)
 {
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* doubles numbu */
+		b = i % 10; /* singles numbu */
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles numbu */
+			d = j % 10; /* singles numbu */
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
 	return (0);
 }
 
