13c7a4422ba301774d5ef25207987ba94d15f66b
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-03-16 01:34:08 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,15 +7,15 @@
 
 void print_alphabet(void)
 {
-      char alpha;
+	char alpha;
 
-	    alpha = 'a';
+	alpha = 'a';
 
-	    while (alpha <= 'z')
-	    {
-		        _putchar(alpha);
-		        alpha++;
-	    }
-	    _putchar('\n');
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
 }
 
