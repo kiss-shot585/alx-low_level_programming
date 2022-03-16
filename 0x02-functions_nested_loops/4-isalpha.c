fb1a04568e7bcf7cfa5f59337ecde144cdd662f2
kiss-shot585 <nkuhrholy33@gmail.com>
2022-03-15 18:08:18 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @c: character to check
+ * Return; 1 if lower case or upper case, 0 if not alphabet character
+ */
+int _isalpha (int c)
+{
+}
+
