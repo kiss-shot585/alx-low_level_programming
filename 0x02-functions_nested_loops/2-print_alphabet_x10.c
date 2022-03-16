150b18012c078ad2e5a64218400e65898bb981b9
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-03-16 01:44:59 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,5 +5,16 @@
  */
 void print_alphabet_x10(void)
 {
+  char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 }
 
