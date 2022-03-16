696adb3b0099a3b25164926c0426747f79d032d2
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-03-16 01:58:06 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,16 @@
  *
  * Return: 1 if lower case, 0 if upper case
  */
+
 int _islower(int c)
 {
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
-
