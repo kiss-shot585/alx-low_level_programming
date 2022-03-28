a356643be767fdfa51dbfe69678610ea28544042
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-03-28 18:34:26 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,11 +7,10 @@
  * @accept: bytes to be accepted
  * Return: pointer to be the byte that matches accept or NULL if not found
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
-	
+
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
