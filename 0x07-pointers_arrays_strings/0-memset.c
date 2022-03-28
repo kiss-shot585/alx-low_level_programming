2c9758f5cdcf614872a1805995f82cd21775521e
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-03-28 17:56:17 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,15 +13,10 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
+	char *ptr = s;
 
-		char *ptr = s;
-
-			while (n--)
-
-						*s++ = b;
-
-				return (ptr);
-
+	while (n--)
+		*s++ = b;
+	return (ptr);
 }
