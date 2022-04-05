32644c72583b83e7cc38b0a65b516be48d6c185f
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-05 02:22:16 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,17 +12,17 @@
  */
 char *argstostr(int ac, char **av)
 {
- char *str;
- int arg, byte, index, size = ac;
-
- if (ac == 0 || av == NULL)
-	 return (NULL);
-
- for (arg = 0; arg < ac; arg++)
- {
+	char *str;
+	int arg, byte, index, size = ac;
+	
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	
+	for (arg = 0; arg < ac; arg++)
+	{
 	 for (byte = 0; av[arg][byte]; byte++)
 		 size++;
- }
+	}
 	str = malloc(sizeof(char) * size + 1);
 	if (str == NULL)
 		return (NULL);
@@ -33,8 +33,6 @@ char *argstostr(int ac, char **av)
 			str[index++] = av[arg][byte];
 		str[index++] = '\n';
 	}
-	
 	str[size] = '\0';
-	
 	return (str);
 }
