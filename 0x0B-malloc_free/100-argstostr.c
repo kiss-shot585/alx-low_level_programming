2f7700599568fc6e93a8c6fc365be84267a23652
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-05 02:19:09 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,34 +12,29 @@
  */
 char *argstostr(int ac, char **av)
 {
-  char *str;
-  int arg, byte, index, size = ac;
+ char *str;
+ int arg, byte, index, size = ac;
 
-  if (ac == 0 || av == NULL)
-    return (NULL);
+ if (ac == 0 || av == NULL)
+	 return (NULL);
 
-  for (arg = 0; arg < ac; arg++)
-    {
-      for (byte = 0; av[arg][byte]; byte++)
-	size++;
-    }
-
-  str = malloc(sizeof(char) * size + 1);
-
-  if (str == NULL)
-    return (NULL);
-
-  index = 0;
-
-  for (arg = 0; arg < ac; arg++)
-    {
-      for (byte = 0; av[arg][byte]; byte++)
-	str[index++] = av[arg][byte];
-
-      str[index++] = '\n';
-    }
-
-  str[size] = '\0';
-
-  return (str);
+ for (arg = 0; arg < ac; arg++)
+ {
+	 for (byte = 0; av[arg][byte]; byte++)
+		 size++;
+ }
+	str = malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+		return (NULL);
+	index = 0;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+		str[index++] = '\n';
+	}
+	
+	str[size] = '\0';
+	
+	return (str);
 }
