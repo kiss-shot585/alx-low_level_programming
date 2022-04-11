9519574963b270ab508751f14782b0be94fddf38
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-11 12:16:45 +0100

    free the dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that free dog
+ * @d:pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
