8853565ffc08da7b4b07e322eae1fedbaeee424f
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-13 07:08:06 +0100

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name : string
+ * @f: function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
