00dc6bddde841abdec238011a4e5e2f7bd9a4af3
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-14 08:55:33 +0100

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,18 @@ int sum_them_all(const unsigned int n, ...)
   unsigned int i;
   va_list list;
   double sum = 0;
-  
-  if (n == 0)
+	
+	if (n == 0)
 	{
-    return (0);
+		return (0);
 	}
 	if (n == 0)
 	{
 		return (0);
 	}
-  
-  va_start(list, n);
-  
+	
+	va_start(list, n);
+	
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
