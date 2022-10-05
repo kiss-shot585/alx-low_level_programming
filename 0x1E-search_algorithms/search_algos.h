f2c6b7808af9dc0753c85a12159dffee5ed1f249
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-10-05 13:09:28 +0100

    head

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,1 +1,12 @@
-header
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+void print_array(int *array, size_t inc, size_t size);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
